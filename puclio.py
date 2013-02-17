138105f0adeeb75dcea6602d6a980461b456f35a
nico <the.vegeta@gmail.com>
2013-02-17 13:30:40 +0100

    Restructuration.

--- puclio.py
+++ puclio.py
@@ -19,7 +19,8 @@ CONFIG_PATH = "~/.config/puclio/config"
 PUTIO_APP_ID = "337"
 PUTIO_TOKEN_PATH = "http://put.io/v2/oauth2/apptoken/" + PUTIO_APP_ID
 
-def setup_account(config):
+def setup_account():
+    config = configparser.ConfigParser()
     tok = input("To get your Oauth token go to: " + PUTIO_TOKEN_PATH +
                 ".\nOauth token ? ")
     config.add_section('account')
@@ -27,35 +28,45 @@ def setup_account(config):
     with open(os.path.expanduser(CONFIG_PATH), 'w') as configfile:
         config.write(configfile)
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description =
+def init_parser():
+    p = argparse.ArgumentParser(description =
             "A Putio Utility, Command LIne Oriented.")
-    subparsers = parser.add_subparsers(dest='cmd')
+    subparsers = p.add_subparsers(dest='cmd')
 
     parser_ls = subparsers.add_parser('ls')
     parser_setup = subparsers.add_parser('setup')
+    return p
+
+def init_account():
+    config = configparser.ConfigParser()
+
+    try:
+        config.read_file(open(os.path.expanduser(CONFIG_PATH)))
+    except FileNotFoundError:
+        print("No configuration file.")
+        setup_account(config)
+
+    token = config['account']['token']
+
+    return putio2.Client(token)
+
+
+if __name__ == "__main__":
+
+    parser = init_parser()
 
     if len(sys.argv) == 1:
         parser.print_help();
         sys.exit(1)
     args = parser.parse_args()
 
-    config = configparser.ConfigParser()
     if args.cmd == 'setup':
-        setup_account(config)
-        exit(1)
-    else:
-        try:
-            config.read_file(open(os.path.expanduser(CONFIG_PATH)))
-        except FileNotFoundError:
-            print("No configuration file.")
-            setup_account(config)
+        setup_account()
+        exit(0)
 
-    o_token = config['account']['token']
+    putio = init_account()
 
     if args.cmd == 'ls':
-        putio = putio2.Client(o_token)
         files = putio.File.list()
         for idx, f in enumerate(files):
             print(str(idx + 1) + "    " + f.name)
